5e24a4d12a26dd9240a1c445ac389328acd15c03
HarshChauhan2601 <144214795+HarshChauhan2601@users.noreply.github.com>
2024-04-24 12:05:19 +0530

    Add files via upload

---
+++ carzking/images/form.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<h1>Carz King - Admin Panel</h1>
+<p>Please, Fill Up all the Details Below :</p>
+
+
+<form action="">
+
+<label for="name">Car Name / Part Name</label>
+<input type="text" name="name" placeholder="Ex. Swift Dezire" required>
+
+
+<label for="cname">Company Name</label>
+<input type="text" name="cname" placeholder="Ex. BMW / Rolls_Royce" required>
+
+<label for="model">Car / Part Model year</label>
+<input type="text" name="model" placeholder="Ex. 2008">
+
+
+
+<label for="type">Type of Car / Spare Part</label>
+<select name="type" id="" required>
+    <option value="" disabled="" >--  Select --</option>
+    <option value="used">Used</option>
+    <option value="new">New</option>
+</select>
+
+<label for=""></label>
+
+<label for=""></label>
+
+
+
+
+</form>
+
+</body>
+</html>
